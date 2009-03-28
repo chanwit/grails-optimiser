bb1eb8847d4d8727d3a76de1fa0228d54461e388
U-LB\chanwit <chanwit@gmail.com>
2009-03-28 23:16:01 +0700

    associated with classregistry to put closure info in

--- src-grails/grails/soot/transformer/ClosureDetector.java
+++ src-grails/grails/soot/transformer/ClosureDetector.java
@@ -2,25 +2,25 @@ package grails.soot.transformer;
 
 import java.util.Map;
 
-import soot.Body;
-import soot.BodyTransformer;
-import soot.Local;
-import soot.RefType;
-import soot.Unit;
+import soot.*;
 import soot.jimple.AssignStmt;
 import soot.jimple.InstanceFieldRef;
 import grails.soot.utils.Helper;
+import grails.soot.reflection.ClassRegistry;
+import grails.soot.reflection.ClassRegistryEntry;
 
 public class ClosureDetector extends BodyTransformer {
 
     @Override
     protected void internalTransform(Body b, String phaseName, Map options) {
 
-//		Extract this pattern
-//		====================
-//    	$r2 = new TestController$_closure1
-//    	specialinvoke $r2.<TestController$_closure1: void <init>(java.lang.Object,java.lang.Object)>(r0, r0)
-//    	r0.<TestController: java.lang.Object index> = $r2
+        ClassRegistry cr = ClassRegistry.v();        
+
+        //      Extract this pattern
+        //      ====================
+        //      $r2 = new TestController$_closure1
+        //      specialinvoke $r2.<TestController$_closure1: void <init>(java.lang.Object,java.lang.Object)>(r0, r0)
+        //      r0.<TestController: java.lang.Object index> = $r2
 
         if (Helper.isConstructor(b)) {
             Unit u = b.getUnits().getFirst();
@@ -31,12 +31,21 @@ public class ClosureDetector extends BodyTransformer {
                 if (!(a.getLeftOp() instanceof InstanceFieldRef)) continue;
                 if (!(a.getRightOp() instanceof Local)) continue;
                 Local right = (Local) a.getRightOp();
+                
                 RefType refType = (RefType) right.getType();
                 if (Helper.isClosureType(refType)) {
                     InstanceFieldRef fr = (InstanceFieldRef) a.getLeftOp();
                     String actionName = fr.getField().getName();
 
-                    // TODO how to use action name (a closure name)
+                    SootClass declaringClass = b.getMethod().getDeclaringClass();
+                    if (cr.containsKey(declaringClass)) {
+                        ClassRegistryEntry cre = cr.get(declaringClass);
+                        cre.addClosure(actionName, refType.getSootClass());
+                    } else {
+                        ClassRegistryEntry cre = new ClassRegistryEntry();
+                        cre.addClosure(actionName, refType.getSootClass());
+                        cr.put(declaringClass, cre);
+                    }
 
                 }
             }
